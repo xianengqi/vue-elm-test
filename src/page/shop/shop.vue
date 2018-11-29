49cb5be9a1007b8f17ecb2712aa8852d37db59dc
xianengqi <920986617@qq.com>
2018-11-29 17:37:34 +0800

    添加shop页面

--- src/page/shop/shop.vue
+++ src/page/shop/shop.vue
@@ -1,3 +1,17 @@
 <template>
   <div>dsada</div>
 </template>
+
+<script>
+import { mapState, mapMutations } from 'vuex'
+import { msiteAddress, shopDetails, foodMenu, getRatingList, ratingScores, ratingtags } from '../../api/getData'
+export default {
+  data () {
+    //
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
